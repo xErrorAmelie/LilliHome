78fc6762486b17c849695eb5d3ac85a0a1f76136
xErrorAmelie <laurikleffel@gmail.com>
2020-07-07 15:41:47 +0200

    Initial commit

---
+++ src/ccameliehome/home.java
@@ -0,0 +1,81 @@
+package ccameliehome;
+
+import java.io.File;
+import org.bukkit.Bukkit;
+import org.bukkit.Effect;
+import org.bukkit.Location;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class home implements CommandExecutor, Listener {
+    ConsoleCommandSender console;
+    @SuppressWarnings("unused")
+    private LilliHome plugin;
+    int i;
+
+    public home(LilliHome plugin) {
+	this.i = 3;
+	this.console = Bukkit.getServer().getConsoleSender();
+	this.plugin = plugin;
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	Player p = (Player) sender;
+	if (cmd.getName().equalsIgnoreCase("home")) {
+	    if (p.hasPermission("lillihome.home") || Files.config.getBoolean(".defaultpermissions") == true) {
+		if (args.length == 1) {
+		    File file = new File("plugins/LilliHome/home/players/" + p.getUniqueId(),
+			    args[0] + ".yml");
+		    YamlConfiguration yamlConfiguration = YamlConfiguration.loadConfiguration(file);
+
+		    if (file.exists()) {
+			double x = yamlConfiguration.getDouble("X");
+			double y = yamlConfiguration.getDouble("Y");
+			double z = yamlConfiguration.getDouble("Z");
+			double yaw = yamlConfiguration.getDouble("Yaw");
+			double pitch = yamlConfiguration.getDouble("Pitch");
+			String worldname = yamlConfiguration.getString("Worldname");
+
+			Location loc = new Location(Bukkit.getWorld(worldname), x, y, z);
+			loc.setPitch((float) pitch);
+			loc.setYaw((float) yaw);
+			p.teleport(loc);
+			p.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
+			p.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
+			p.getWorld().playEffect(loc, Effect.ENDER_SIGNAL, 0);
+			p.getWorld().playEffect(loc, Effect.MOBSPAWNER_FLAMES, 0);
+			p.playSound(loc, Sound.ENTITY_ENDERMAN_TELEPORT, 1, 1);
+			if (Files.config.getString(".Language").contains("de")) {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.de.getString(".athomemessage").replace("%home%", args[0]).replace("&", "§"));
+			} else {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.en.getString(".athomemessage").replace("%home%", args[0]).replace("&", "§"));
+			}
+		    } else {
+			if (Files.config.getString(".Language").contains("de")) {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.de.getString(".homenotexistmessage").replace("%home%", args[0]).replace("&", "§"));
+			} else {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.en.getString(".homenotexistmessage").replace("%home%", args[0]).replace("&", "§"));
+			}
+		    }
+		} else {
+		    if (Files.config.getString(".Language").contains("de")) {
+			p.sendMessage(String.valueOf(LilliHome.prefix) + Files.de.getString(".homeusemessage").replace("&", "§"));
+		    } else {
+			p.sendMessage(String.valueOf(LilliHome.prefix) + Files.en.getString(".homeusemessage").replace("&", "§"));
+		    }
+		}
+	    }
+	}
+	return false;
+    }
+}
---
+++ src/ccameliehome/homelist.java
@@ -0,0 +1,111 @@
+package ccameliehome;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import net.md_5.bungee.api.ChatColor;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class homelist implements CommandExecutor, Listener {
+
+    public homelist(LilliHome plugin) {
+	this.console = Bukkit.getServer().getConsoleSender();
+	this.plugin = plugin;
+    }
+
+    ConsoleCommandSender console;
+    private LilliHome plugin;
+
+    private static String removeLastChar(String str, Player p) {
+
+	return str.substring(0, str.length() - 5);
+
+    }
+
+    HashMap<String, Integer> homeamount = new HashMap<String, Integer>();
+    HashMap<String, Integer> homeamountnull = new HashMap<String, Integer>();
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	Player p = (Player) sender;
+
+	if (label.equalsIgnoreCase("homelist")) {
+
+	    if (p.hasPermission("lillihome.homelist") || Files.config.getBoolean(".defaultpermissions") == true) {
+		if (sender instanceof Player) {
+		    (new BukkitRunnable() {
+			public void run() {
+			    File folder = new File("plugins/LilliHome/home/players/" + p.getUniqueId());
+			    if (folder.exists()) {
+
+				String[] fileNames = folder.list();
+
+				StringBuilder sb = new StringBuilder();
+
+				for (int i = 0; i < fileNames.length; i++) {
+				    if (fileNames.length == i) {
+					sb.append(fileNames[i]);
+				    }
+				    sb.append(fileNames[i]).append(ChatColor.GRAY + ", " + ChatColor.GOLD);
+				    homeamount.put(p.getName(), i+1);
+				}
+
+				String msg = sb.toString().trim().replace(".yml", "");
+				if (!(removeLastChar(msg, p).length() <= 0)) {
+				    if (Files.config.getString(".Language").contains("de")) {
+					p.sendMessage(String.valueOf(LilliHome.prefix) + Files.de
+						.getString(".homelistmessage")
+						.replace("%amount%", "" + homeamount.get(p.getName())).replace("&", "§")
+						+ removeLastChar(msg, p));
+				    } else {
+					p.sendMessage(String.valueOf(LilliHome.prefix) + Files.en
+						.getString(".homelistmessage")
+						.replace("%amount%", "" + homeamount.get(p.getName())).replace("&", "§")
+						+ removeLastChar(msg, p));
+				    }
+				} else {
+				    if (Files.config.getString(".Language").contains("de")) {
+					p.sendMessage(String.valueOf(LilliHome.prefix)
+						+ Files.de.getString(".nohomemessage").replace("&", "§"));
+				    } else {
+					p.sendMessage(String.valueOf(LilliHome.prefix)
+						+ Files.en.getString(".nohomemessage").replace("&", "§"));
+				    }
+				}
+			    } else {
+				if (Files.config.getString(".Language").contains("de")) {
+				    p.sendMessage(String.valueOf(LilliHome.prefix)
+					    + Files.de.getString(".nohomemessage").replace("&", "§"));
+				} else {
+				    p.sendMessage(String.valueOf(LilliHome.prefix)
+					    + Files.en.getString(".nohomemessage").replace("&", "§"));
+				}
+			    }
+			}
+		    }).runTaskLater(this.plugin, 20L);
+		} else {
+		    p.sendMessage("You cannot send this command in a console!");
+		}
+	    } else {
+		if (Files.config.getString(".Language").contains("de")) {
+		    p.sendMessage(String.valueOf(LilliHome.prefix)
+			    + Files.de.getString(".nopermissionmessage").replace("&", "§"));
+		} else {
+		    p.sendMessage(String.valueOf(LilliHome.prefix)
+			    + Files.en.getString(".nopermissionmessage").replace("&", "§"));
+		}
+	    }
+
+	}
+	return false;
+    }
+}
