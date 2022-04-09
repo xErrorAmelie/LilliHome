3c313ac3de1ce3a6732246b494a92d3e05b38119
DreamyLynn <59802880+DreamyLynn@users.noreply.github.com>
2022-04-09 22:33:53 +0200

    Change to IntelliJ Project

---
+++ src/main/java/ccamelie/lillihome/Files.java
@@ -0,0 +1,75 @@
+package ccamelie.lillihome;
+
+import java.io.File;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class Files implements CommandExecutor {
+
+    public static File deFile;
+    public static FileConfiguration de;
+    public static File enFile;
+    public static FileConfiguration en;
+    public static File configFile;
+    public static FileConfiguration config;
+
+    public static void x(LilliHome f) {
+	if (!f.getDataFolder().exists()) {
+	    f.getDataFolder().mkdirs();
+	}
+
+	deFile = new File(f.getDataFolder(), "de.yml");
+	if (!deFile.exists()) {
+	    f.saveResource("de.yml", false);
+	}
+	de = YamlConfiguration.loadConfiguration(deFile);
+
+	enFile = new File(f.getDataFolder(), "en.yml");
+	if (!enFile.exists()) {
+	    f.saveResource("en.yml", false);
+	}
+	en = YamlConfiguration.loadConfiguration(enFile);
+
+	configFile = new File(f.getDataFolder(), "config.yml");
+	if (!configFile.exists()) {
+	    f.saveResource("config.yml", false);
+	}
+	config = YamlConfiguration.loadConfiguration(configFile);
+
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	Player p = (Player) sender;
+	if (cmd.getName().equalsIgnoreCase("lillihome")) {
+	    if (p.isOp() || p.hasPermission("lillihome.admin.reload")) {
+		if (args.length == 1) {
+		    if (args[0].contains("reload")) {
+			de = YamlConfiguration.loadConfiguration(deFile);
+			en = YamlConfiguration.loadConfiguration(enFile);
+			config = YamlConfiguration.loadConfiguration(configFile);
+			p.sendMessage(LilliHome.prefix + ChatColor.GRAY + "Reload complete");
+		    }
+
+		} 
+	    }else {
+		if (Files.config.getString(".Language").contains("de")) {
+		    p.sendMessage(
+			    String.valueOf(LilliHome.prefix) + Files.de.getString(".nopermissionmessage").replace("&", "§"));
+		} else {
+		    p.sendMessage(
+			    String.valueOf(LilliHome.prefix) + Files.en.getString(".nopermissionmessage").replace("&", "§"));
+		}
+	    }
+	}else {
+	    p.sendMessage(LilliHome.prefix + ChatColor.GRAY + "/lillihome reload");
+	}
+	return false;
+    }
+}
---
+++ src/main/java/ccamelie/lillihome/delhome.java
@@ -0,0 +1,66 @@
+package ccamelie.lillihome;
+
+import java.io.File;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class delhome implements CommandExecutor, Listener {
+    public delhome(LilliHome plugin) {
+	this.console = Bukkit.getServer().getConsoleSender();
+	this.plugin = plugin;
+    }
+
+    ConsoleCommandSender console;
+    @SuppressWarnings("unused")
+    private LilliHome plugin;
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+	Player p = (Player) sender;
+	if (cmd.getName().equalsIgnoreCase("delhome")) {
+	    if (p.hasPermission("lillihome.delhome") || Files.config.getBoolean(".defaultpermissions") == true) {
+		if (args.length == 1) {
+		    File file = new File("plugins/LilliHome/home/players/" + p.getUniqueId(),
+			    args[0] + ".yml");
+		    if (file.exists()) {
+			file.delete();
+			if (Files.config.getString(".Language").contains("de")) {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.de.getString(".homedeletedmessage").replace("%home%", args[0]).replace("&", "§"));
+			} else {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.en.getString(".homedeletedmessage").replace("%home%", args[0]).replace("&", "§"));
+			}
+		    } else {
+			if (Files.config.getString(".Language").contains("de")) {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.de.getString(".homedeletedmessageerror").replace("%home%", args[0]).replace("&", "§"));
+			} else {
+			    p.sendMessage(String.valueOf(LilliHome.prefix)
+				    + Files.en.getString(".homedeletedmessageerror").replace("%home%", args[0]).replace("&", "§"));
+			}
+		    }
+		} else {
+		    if (Files.config.getString(".Language").contains("de")) {
+			p.sendMessage(String.valueOf(LilliHome.prefix) + Files.de.getString(".delhomeusemessage").replace("&", "§"));
+		    } else {
+			p.sendMessage(String.valueOf(LilliHome.prefix) + Files.en.getString(".delhomeusemessage").replace("&", "§"));
+		    }
+		}
+	    }else {
+		if (Files.config.getString(".Language").contains("de")) {
+		    p.sendMessage(
+			    String.valueOf(LilliHome.prefix) + Files.de.getString(".nopermissionmessage").replace("&", "§"));
+		} else {
+		    p.sendMessage(
+			    String.valueOf(LilliHome.prefix) + Files.en.getString(".nopermissionmessage").replace("&", "§"));
+		}
+	    }
+	}
+	return true;
+    }
+}
